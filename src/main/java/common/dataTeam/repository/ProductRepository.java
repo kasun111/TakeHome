520fb330b533325e38b957be57f05a28164cc061
kasun111 <43335544+kasun111@users.noreply.github.com>
2024-03-29 22:33:38 +0530

    limited query results to 100

--- src/main/java/common/dataTeam/repository/ProductRepository.java
+++ src/main/java/common/dataTeam/repository/ProductRepository.java
@@ -1,6 +1,7 @@
 package common.dataTeam.repository;
 
 import common.dataTeam.entity.Product;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -11,5 +12,5 @@ import java.util.List;
 @Repository
 public interface ProductRepository extends JpaRepository <Product, String>{
     @Query("SELECT p FROM Product p JOIN ShopperProducts sp ON p.productId = sp.productId WHERE sp.shopperId = :shopperId AND p.category = :category AND p.brand = :brand")
-    List<Product> findAllByShopperIdAndCategoryAndBrand(@Param("shopperId") String shopperId, @Param("category") String category, @Param("brand") String brand);
+    List<Product> findAllByShopperIdAndCategoryAndBrand(@Param("shopperId") String shopperId, @Param("category") String category, @Param("brand") String brand, Pageable pageable);
 }
--- src/main/java/common/personalizedData/personalizedDataService/personalizedDataServiceImpl/DataServiceImpl.java
+++ src/main/java/common/personalizedData/personalizedDataService/personalizedDataServiceImpl/DataServiceImpl.java
@@ -6,6 +6,8 @@ import common.personalizedData.personalizedDataModel.RequestDTO;
 import common.personalizedData.personalizedDataModel.ResponseDTO;
 import common.personalizedData.personalizedDataService.DataService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -20,8 +22,9 @@ public class DataServiceImpl implements DataService {
     public ResponseDTO productsByShopper(RequestDTO request) {
         ResponseDTO responseDTO = new ResponseDTO();
         try {
+            Pageable pageable = PageRequest.of(0, 100);
             List<Product> productsList = productRepository.findAllByShopperIdAndCategoryAndBrand(request.getShopperId(),
-                    request.getCategory(), request.getBrand());
+                    request.getCategory(), request.getBrand(), pageable);
             responseDTO.setStatus("SUCCESS");
             responseDTO.setProducts(productsList);
         } catch (Exception e) {
